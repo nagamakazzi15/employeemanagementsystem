f646eed9e3ffbb09f2193dc6780d4359b6174afe
nagamakazzi15 <nagamakazzi15@example.com>
2022-08-21 20:29:26 -0700

    first Commit

---
+++ src/main/java/com/mit/employeemangmentsystem/Employeemangmentsystem1Application.java
@@ -0,0 +1,13 @@
+package com.mit.employeemangmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeemangmentsystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeemangmentsystem1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mit/employeemangmentsystem/Employeemangmentsystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mit.employeemangmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employeemangmentsystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
